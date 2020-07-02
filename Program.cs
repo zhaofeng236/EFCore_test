1afc474796687b9cf5ac1b7c5d71ffe5579347c1
feng_work <zhaofeng236@outlook.com>
2020-07-02 10:44:16 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EFCore_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
